1bf6c788414ed1f5420efacebdc661ee0081afe4
premchandra <prem.leishangthem@cision.com>
2020-06-16 14:07:46 +0530

    Odata working - original odata flavour

---
+++ ODataExperiment/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using ODataExperiment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataExperiment.Controllers
+{
+
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>
+            {
+                CreateNewStudent("Cody Allen", 130),
+                CreateNewStudent("Todd Ostermeier", 160),
+                CreateNewStudent("Viral Pandya", 140)
+            };
+        }
+
+        private static Student CreateNewStudent(string name, int score)
+        {
+            return new Student
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Score = score
+            };
+        }
+    }
+}
